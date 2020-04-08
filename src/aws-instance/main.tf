e125a895bb9e37dd3e2c2981428009befb4ba623
Yurii Soltys <y.soltys@scalr.com>
2020-04-08 16:03:59 +0300

    Create main.tf

---
+++ src/aws-instance/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "default" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  tags = {
+    Name      = "test-it"
+    timestamp = timestamp()
+  }
+
+  lifecycle {
+    ignore_changes = ["tags"]
+  }
+}
