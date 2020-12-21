17db9bc6d86bf8c86beef280290adbaa9893f968
Yurii Soltys <y.soltys@scalr.com>
2020-12-21 18:57:50 +0200

    Update main.tf

--- tf-revizor-fixtures/aws/main.tf
+++ tf-revizor-fixtures/aws/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
-    name   = "name"
+    name   = "name2"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   filter {
