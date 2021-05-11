d0d77ac739bc85e007fc060e98157c04cd1e08a8
Yurii Soltys <y.soltys@scalr.com>
2021-05-11 17:35:11 +0300

    Update main.tf

--- tf-revizor-fixtures/aws/main.tf
+++ tf-revizor-fixtures/aws/main.tf
@@ -7,6 +7,7 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+  
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
