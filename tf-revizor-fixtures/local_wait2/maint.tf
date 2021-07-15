b529e3387317f0d12a5e1311b86f698aac4710d6
Yurii Soltys <y.soltys@scalr.com>
2021-07-15 15:14:27 +0300

    Create maint.tf

---
+++ tf-revizor-fixtures/local_wait2/maint.tf
@@ -0,0 +1,37 @@
+variable "run_id" {
+  default = "sdsdsd"
+}
+
+variable "sleep_time" {
+  default = 30
+}
+
+resource "random_integer" "timeout" {
+  min = 30
+  max = 180
+
+  keepers = {
+    run_id = var.run_id
+  }
+}
+
+resource "null_resource" "wait" {
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "sleep ${var.sleep_time}"
+  }
+}
+
+
+
+output "very_long" {
+  value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
+}
+
+output "senc_out" {
+  value = "secret data"
+  description = "my sensitive output"
+  sensitive = true
+}
