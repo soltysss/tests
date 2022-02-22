50940e8901990ef63dbca23c10779564eebaadf6
Yurii Soltys <y.soltys@scalr.com>
2022-02-22 09:57:12 +0200

    Update main.tf

--- tf-revizor-fixtures/local_wait/main.tf
+++ tf-revizor-fixtures/local_wait/main.tf
@@ -1,3 +1,13 @@
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "8.5.8"
+        }
+    }
+}
+
+
 variable "run_id" {
   default = "sdsdsd"
 }
