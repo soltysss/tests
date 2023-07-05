d95139a2afb81bafe5d450600f837255a168b963
Yurii Soltys <y.soltys@scalr.com>
2023-07-05 16:36:41 +0300

    Update maint.tf

--- tf-revizor-fixtures/local_wait2/maint.tf
+++ tf-revizor-fixtures/local_wait2/maint.tf
@@ -15,6 +15,10 @@ variable "sleep_time" {
   default = 30
 }
 
+data "scalr_vcs_provider" "vcs" {
+  name = "sts"
+}
+
 resource "random_integer" "timeout" {
   min = 30
   max = 180
@@ -33,16 +37,13 @@ resource "null_resource" "wait" {
   }
 }
 
-data "scalr_vcs_provider" "example" {
-  name = "sts"
-}
 
 output "very_long" {
   value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
 }
 
 output "provider_id" {
-  value = data.example.id
+  value = data.vcs.id
 }
 
 output "senc_out" {
