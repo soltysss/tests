3b744a1f49181b64a52c62320b318d7b34f8a207
Yurii Soltys <y.soltys@scalr.com>
2023-07-05 16:31:22 +0300

    Update maint.tf

--- tf-revizor-fixtures/local_wait2/maint.tf
+++ tf-revizor-fixtures/local_wait2/maint.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+        }
+    }
+}
+
+
 variable "run_id" {
   default = "sdsdsd"
 }
@@ -24,10 +33,16 @@ resource "null_resource" "wait" {
   }
 }
 
-
+data "scalr_vcs_provider" "example" {
+  name = "sts"
+}
 
 output "very_long" {
   value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
+}
+
+output "provider_id" {
+  value = data.example.id
 }
 
 output "senc_out" {
