ada31bef6572febf941656f96f80988ca7095d81
soltysss <y.soltys@scalr.com>
2024-03-06 12:15:19 +0200

    submodules

---
+++ tf-revizor-fixtures/local_wait_submodule/main.tf
@@ -0,0 +1,73 @@
+
+variable "run_id" {
+  default = "run-jyslljjfs84jsl111127777888"
+}
+
+
+variable "sleep_time" {
+  default = 5
+}
+
+variable "sens" {
+  default   = "xxxxxx"
+  sensitive = true
+}
+
+resource "random_integer" "timeout" {
+  min = 30
+  max = 180
+
+  keepers = {
+    run_id = "Possibly sensitive ${var.run_id}!!"
+  }
+}
+
+resource "null_resource" "wait" {
+  triggers = {
+      run_id = "This can be sensitive ${var.run_id}!"
+      name   = var.sens
+  }
+  provisioner "local-exec" {
+    command = "sleep ${var.sleep_time}"
+     environment = {
+       FOO = "bar"
+       BAR = 1
+       BAZ = "true"
+     }
+  }
+}
+
+module "local-wait" {
+  source = "./modules/tf-revizor-fixtures/terraform-null-module"
+  secret = var.sens
+}
+
+output "very_long" {
+  value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+}
+
+output "test" {
+  value = "laborum ${var.sens}"
+  sensitive = true
+}
+
+output "senc_out" {
+  value       = "xxxxxx"
+  description = "my sensitive output"
+  sensitive   = true
+}
+
+/*
+output "module_res" {
+  value = "${module.local-wait.the_id} xxxxxx"
+  sensitive = true
+}
+*/  
+output "sleeped_for" {
+  value = "${var.sleep_time} this is suppose to be sensitive"
+}
+
+output "run_id" {
+  value = "${var.run_id} this is suppose to be sensitive. real value is 555555"
+}
+  
