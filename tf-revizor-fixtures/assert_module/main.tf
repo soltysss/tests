c5e05b9620f70097a8f5bbe2c19283303560b1b9
soltyssss <y.soltys@scalr.com>
2024-03-22 17:43:17 +0200

    assert module with wiat

--- tf-revizor-fixtures/assert_module/main.tf
+++ tf-revizor-fixtures/assert_module/main.tf
@@ -1,1 +1,25 @@
- 
+
+resource "null_resource" "wait" {
+  triggers = {
+      run_id = "Bla"
+      name   = "bla"
+  provisioner "local-exec" {
+    command = "sleep 1"
+     environment = {
+       FOO = "bar"
+       BAR = 1
+       BAZ = "true"
+     }
+  }
+}
+
+module "assertion_unix_only" {
+  source  = "Invicton-Labs/assertion/null"
+
+  // The condition to ensure is TRUE
+  // In this example, assert that the operating system uses forward slashes for path separators
+  condition = "/" == "/"
+
+  // The error message to print out if the condition evaluates to FALSE
+  error_message = "This Terraform configuration can only be run on Unix-based machines."
+}
