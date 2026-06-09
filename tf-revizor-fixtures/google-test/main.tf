789daaee205bfc62eda122ff77710ad7fe62451f
soltysss <y.soltys@scalr.com>
2026-06-09 13:22:55 +0300

    google reosurce default lables in config

--- tf-revizor-fixtures/google-test/main.tf
+++ tf-revizor-fixtures/google-test/main.tf
@@ -10,9 +10,12 @@
   provider "google" {
     project = "sacred-epigram-480016-u0"
     region  = "us-central1"
+    default_labels = {
+        my_global_keys = "one"
+        my_default_key = "two"
+    }
   }
 
   resource "google_pubsub_topic" "test" {
     name = "scalr-labels-test"
   }
-
