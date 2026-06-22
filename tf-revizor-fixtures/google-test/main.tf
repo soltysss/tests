4d1159c730e4301c78e15ffefcb2bb13e71e1e2f
soltysss <y.soltys@scalr.com>
2026-06-22 12:10:23 +0300

    just test

--- tf-revizor-fixtures/google-test/main.tf
+++ tf-revizor-fixtures/google-test/main.tf
@@ -17,10 +17,10 @@ locals {
 provider "google" {
   project = "sacred-epigram-480016-u0"
   region  = "us-central1"
-  default_labels = merge(local.env_labels, {
+  default_labels = {
     my_global_keys = "one"
     my_default_key = "two"
-  })
+  }
 }
 
 resource "google_pubsub_topic" "test" {
